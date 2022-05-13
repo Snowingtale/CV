413c53e2b99779a442dc3158afa6298e5a559fcd
unknown <george.scarlat97@gmail.com>
2022-05-13 03:40:04 +0300

    design 0.0.0.1

--- cv/src/app/app.component.ts
+++ cv/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GuiComponent } from './gui/gui.component';
 
 @Component({
   selector: 'app-root',
--- cv/src/app/app.module.ts
+++ cv/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GuiComponent } from './gui/gui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuiComponent
   ],
   imports: [
     BrowserModule,
